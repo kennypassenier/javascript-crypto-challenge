9f3e2757cbbbd77e55b8e8a2bebb0248de9161c9
Kenny Passenier <kennypassenier@gmail.com>
2020-10-30 02:18:58 +0100

    Signature passes the tests

---
+++ src/Signature.js
@@ -0,0 +1,15 @@
+// Docs: https://libsodium.gitbook.io/doc/public-key_cryptography/public-key_signatures
+// Include dependencies
+const nacl = require('libsodium-wrappers');
+
+module.exports = async () => {
+    await nacl.ready;
+    // Create a public and private key and store them into a variable
+    const key = nacl.crypto_sign_keypair();
+    return ({
+        verifyingKey: key.publicKey,
+        sign: (msg) => {
+            return nacl.crypto_sign(msg, key.privateKey);
+        }
+    });
+}
